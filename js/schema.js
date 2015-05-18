37b891abaa81170101428c58530c1c6589b6dde2
Brian Craft <bcboy@thecraftstudio.com>
2015-05-18 08:13:04 -0700

    Add schema options, doc & title strings.

--- js/schema.js
+++ js/schema.js
@@ -9,34 +9,44 @@ function key(v, k) {
 }
 
 var S = module.exports = function (obj) {
-    return ['object', ..._.map(obj, key)];
+    return ['object', {}, ..._.map(obj, key)];
 };
+
+// merge, dropping undefined props
+var p = (...objs) => _.pick(_.extend.apply(null, objs), (v, k) => v !== undefined);
 
 // how to represent
 //   required (we really want to tag 'optional', not 'required')
-//   doc
-//   title
 var methods = {
     string: function (value) {
         var val = _.isString(value) ? ['value', value] :
             (_.isRegExp(value) ? ['pattern', value] : []);
-        return ['string', val];
+        return ['string', {}, val];
     },
     number: function (value) {
         var val = _.isArray(value) ?  ['interval', value] :
             (_.isNumber(value) ? ['value', value] : []);
-        return ['number', val];
+        return ['number', {}, val];
     },
     or: function (...args) {
-        return ['or', ...args];
+        return ['or', {}, ...args];
     },
     array: function (...args) {
-        return ['array', ['fixed', ...args]];
-    }
+        return ['array', {}, ['fixed', ...args]];
+    },
+    d: function (...args) {
+        if (args.length === 2) {
+            args.unshift(undefined);
+        }
+        var [title, description, [type, opts, ...rest]] = args;
+        return [type,
+               p({title: title, description: description}, opts),
+               ...rest];
+    },
 };
 
 methods.array.of = function (sch) {
-    return ['array', ['pattern', sch]];
+    return ['array', {}, ['pattern', sch]];
 };
 
 _.extend(S, methods);
