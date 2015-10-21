6a9e236af8a77dbb6a53df62f741dfb20b88a699
Brian Craft <craft@soe.ucsc.edu>
2015-10-21 16:08:11 -0700

    Support null values in schema.

--- js/schema.js
+++ js/schema.js
@@ -56,6 +56,7 @@ var methods = {
                m({title: title, description: description}, opts),
                ...rest];
     },
+	nullval: ['null', {}]
 };
 
 methods.array.of = function (schema) {
