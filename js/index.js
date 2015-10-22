30040fe55cc25f0c9dff466cdbbb99f8d4a89cea
Brian Craft <craft@soe.ucsc.edu>
2015-10-22 13:31:06 -0700

    demo schema & render.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,20 @@
-/*global console: false */
+/*global console: false, require: false, document: false */
 'use strict';
-console.log('hello');
-var f = x => 2 * x;
-console.log(f(5));
 
-// destructuring with default
-function g({x = 6}) {
-	console.log('x', x);
-}
+var demo = require('./demo-schema');
+var {toHTML, css} = require('./html');
+var main = document.getElementById('main');
+var _ = require('underscore');
 
-// destructuring with default for intermediate value
-function h({x: {y} = {}}) {
-	console.log('y', y);
-}
+var top = _.values(demo);
 
-g({x: 10});
-g({});
-h({x: {y: 7}});
-h({});
-h({x: {z: 12}});
-h({x: {y: 12}});
+var docs = _.map(top, s => toHTML(s, top)).join('<br>');
+var page =
+`<html>
+    <body>
+    ${css}
+	${docs}
+    </body>
+</html>`;
+
+main.innerHTML = page;
