e82ad284a54338d706bd4d51885b7407bef54b63
Brian Craft <bcboy@thecraftstudio.com>
2015-10-25 15:29:48 -0700

    Fix dist paths.

---
+++ index.js
@@ -0,0 +1,7 @@
+/*global module: false, require: false */
+'use strict';
+module.exports = {
+	schema: require('./dist/schema'),
+	html: require('./dist/html'),
+	jsonSchema: require('./dist/jsonSchema')
+};
